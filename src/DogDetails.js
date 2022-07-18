a82b08006da39370f8556a43458540c9b5ded826
Alex Linden <alextl2004@gmail.com>
2022-07-18 16:32:36 -0700

    add photos to src

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,16 +1,22 @@
 import React from "react"
 import { useParams } from "react-router-dom";
+import whiskey from "./whiskey.jpg"
+import duke from "./duke.jpg"
+import perry from "./perry.jpg"
+
 
 function DogDetails({dog}){
   const params = useParams();
+  const dogImages = [whiskey, duke, perry]
 
+  console.log("DogDetails ====", dog)
 return(
   <div>
     <h3>{dog.name}</h3>
     <h3>Age: {dog.age}</h3>
-    <img src={"./whiskey"} alt={dog.name} />
+    <img src={`./${dog.src}.jpg`} alt={dog.name} />
     <ul>
-      {dog.facts.map(f => <h5>{f}</h5>)}
+      {dog.facts.map((f, idx) => <h5 key={idx}>{f}</h5>)}
     </ul>
   </div>
 )
